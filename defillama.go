3f786b16ff3a987b919618943e2e219367e78ec0
Riley Snyder <rileysnyder@protonmail.ch>
2023-06-12 16:55:04 -0500

    feat: init

---
+++ defillama.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+const (
+	DefiLlamaTVLURL = "https://api.llama.fi/tvl/%s"
+)
+
+func GetTVL(protocol string) (result float64, err error) {
+
+	req, err := http.NewRequest("GET", fmt.Sprintf(DefiLlamaTVLURL, protocol), nil)
+	if err != nil {
+		return result, err
+	}
+
+	req.Header.Add("User-Agent", "Mozilla/5.0")
+	req.Header.Add("accept", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return result, err
+	}
+
+	results, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return result, err
+	}
+
+	return strconv.ParseFloat(string(results), 32)
+}
